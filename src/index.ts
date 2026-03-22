fdf0d368e96ba39642a2de8e9083d1e803500c83
Sergio Junior <sergiaraujo15@gmail.com>
2026-03-22 11:40:58 -0300

    feat(express): faz o express usar o taskRoutes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from "express";
+import { taskRoutes } from "./routes/task.routes";
 
 const app = express();
 const PORT = 3333;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.status(200).json({ mensagem: "API funcionando!" });
-});
+app.use("/tasks", taskRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
